1fa8d4f6aa61ae34235102506055411cd21c44a2
ginnanikasuos <e.onzuka@gmail.com>
2021-12-05 10:06:56 +0900

    Initial Commit

---
+++ VisionAndNFC/VisionAndNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionAndNFCApp.swift
+//  VisionAndNFC
+//
+//  Created by 隠塚永治 on 2021/12/05.
+//
+
+import SwiftUI
+
+@main
+struct VisionAndNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
